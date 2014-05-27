965e508c3cc1c1bc0adf44725208b9bba2b44c19
leandro_marcineiro <leandro_marcineiro@10.1.48.124>
2014-05-27 14:28:37 -0300

    inicialização de um projeto

---
+++ src/teste/TESTE.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author leandro_marcineiro
+ */
+public class TESTE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null,":)");
+    }
+}
